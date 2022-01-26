62c4bbf3a133a1c2807db547d16e76d30f6e7967
ginstone <hjg719@139.com>
2022-01-26 09:38:30 +0800

    种菜记录 - 编辑、删除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <el-container direction="vertical">
     <el-main>
-      <el-button type="success" @click="showDialog=true">添加</el-button>
+      <el-button type="success" @click="showDialog=true;this.dialogTitle='添加'">添加</el-button>
       <el-pagination
           :current-page.sync="params.page"
           :default-current-page="1"
@@ -17,12 +17,13 @@
         <el-table-column label="记录时间" width="100px">
           <template #default="s">
             <my-timestamp :time="s.row.timeR" />
+            <el-icon color="red" class="delete-icon" @click="deleteRecord(s.row.uuid)"><delete-filled /></el-icon>
           </template>
         </el-table-column>
         <el-table-column label="操作时间" width="100px">
           <template #default="s">
             <my-timestamp :time="s.row.timeO" />
-          </template>
+            <el-icon color="red" class="delete-icon" @click="editRecord(s.row)"><edit /></el-icon> </template>
         </el-table-column>
         <el-table-column label="地块" prop="position" width="80px" />
         <el-table-column label="操作" prop="typeO" width="80px" />
@@ -93,11 +94,13 @@
 // @ is an alias to /src
 import {mapActions} from "vuex";
 import MyTimestamp from "@/components/my/my-timestamp";
-import {reactive} from "vue";
+import {DeleteFilled, Edit} from "@element-plus/icons-vue";
+import {ElMessage, ElMessageBox} from "element-plus";
+import {copyObj} from "@/assets/js/utils/ObjectUtils";
 
 export default {
   name: 'Home',
-  components: {MyTimestamp},
+  components: {MyTimestamp,DeleteFilled,Edit},
   computed: {},
   data() {
     return {
@@ -131,6 +134,16 @@ export default {
   },
   methods: {
     ...mapActions("record", [`add`, `page`, `del`, `getOptions`, `update`]),
+    deleteRecord(uuid){
+      ElMessageBox.confirm("确认删除？","确认删除这条记录？",{type:'warning','button-size':'small'}).then(()=>{
+        this.del(uuid).then(()=>{ElMessage.success("删除成功");this.refresh()})
+      })
+    },
+    editRecord(item){
+      this.record = copyObj(item)
+      this.showDialog =true;
+      this.dialogTitle='修改'
+    },
     async submit() {
       if (this.dialogTitle === '添加') {
         const res = await this.add(this.record);
@@ -164,5 +177,8 @@ export default {
 .tips{
   color:red;
   margin-left: 5px;
+}
+.delete-icon{
+  cursor: pointer;
 }
 </style>
